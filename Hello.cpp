db83511578132f0d0729d0ef6cc0549eab7f933d
DevinSigley <devinsigley@gmail.com>
2017-06-20 00:40:53 -0400

    Testing switching of .git folder

--- Hello.cpp
+++ Hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// This is part of the TestBranch code
+// Comment changed to test switching of .git folder
 int main() {
 	cout << "Hello World\n";	// prints "Hello World" to the console
 	cin.ignore();				// used to keep console open until input
