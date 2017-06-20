a65f9d945bed6f50d9cd792996bc4866d46b2c9d
DevinSigley <devinsigley@gmail.com>
2017-06-20 00:44:38 -0400

    Commit #1

--- Hello.cpp
+++ Hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// Commit revision #1
 // Comment changed to test switching of .git folder
 int main() {
 	cout << "Hello World\n";	// prints "Hello World" to the console
