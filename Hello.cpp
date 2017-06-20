d63d998659a326d3a4aa3e33b9f2255efc49f3ec
DevinSigley <devinsigley@gmail.com>
2017-06-19 23:49:51 -0400

    Added files to remote repo. 1st test

---
+++ Hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// This is part of the TestBranch code
+int main() {
+	cout << "Hello World\n";	// prints "Hello World" to the console
+	cin.ignore();				// used to keep console open until input
+	return 0;
+}
